2c700f64ad254eb294ac0a50e1b6ea6112e0d6d5
danieladavadilla <72984059+danieladavadilla@users.noreply.github.com>
2022-08-05 21:54:04 +0800

    Add files via upload

---
+++ exercise2.m
@@ -0,0 +1,16 @@
+f = imread('fruits.png');
+figure(1), imshow(f);
+whos f #DATA TYPE IS uint8
+imfinfo 'fruits.png'
+
+pkg load image
+g = imresize(f,[512 610]);
+imwrite(g, 'fruits2.png');
+h = imread('fruits2.png');
+figure(2), imshow(h);
+imfinfo 'fruits2.png'
+
+hsv_fruits = rgb2hsv(f);
+imwrite(hsv_fruits, 'fruits3.png');
+h = imread('fruits3.png');
+figure(3), imshow(h);
